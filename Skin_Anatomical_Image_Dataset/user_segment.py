f83e080fb05a9a398cb9954d3f53c387a976383a
Carwyn Collinsworth <carwyn.collinsworth@stonybrook.edu>
2023-02-10 19:17:09 -0500

    Added VIA, updated README

---
+++ Skin_Anatomical_Image_Dataset/user_segment.py
@@ -0,0 +1,39 @@
+# Unfinished as "VIA" (VGG Image Annotator) does this function
+
+# This file should take all images in all folders in "Image/" folder, and via user input, create a masked image, and save in the directory structure.
+
+
+
+'''
+Directory structure:
+
+Sample vs Complex?
+
+Simple:
+
+Train/
+ - Image/ - stores original image
+  - image1.jpg
+  - image2.jpg
+  - ...
+
+ - instance/
+  - who knows
+
+ - Semantic/ - stores mask
+  - image1.png
+  - maybeimage2.png
+  - ...
+ - InstCategory.json
+
+'''
+
+
+def full_mask_process():
+    pass
+
+def main():
+    print("hi")
+
+if __name__ == "__main__":
+    main()
