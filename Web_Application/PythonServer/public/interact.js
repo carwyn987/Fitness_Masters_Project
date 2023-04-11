40f24de24c36e2d86f944416e2d095ed3d3a5b18
Carwyn Collinsworth <carwyn.collinsworth@stonybrook.edu>
2023-04-10 22:58:02 -0400

    Finished input images for gui. Now need to get them all sending to server.

--- Web_Application/PythonServer/public/interact.js
+++ Web_Application/PythonServer/public/interact.js
@@ -1,19 +1,20 @@
-import { make_gaussian_blur_request } from './requests.js';
-
 window.addEventListener('load', function() {
-    document.querySelector('input[type="file"]').addEventListener('change', function(event) {
+
+    // Adding event listener to the submit button
+    document.getElementById('submit').addEventListener('click', function(event) {
+        console.log("submit button clicked")
         event.preventDefault();
-        if (this.files && this.files[0]) {
-            var img = document.querySelector('img');
+        const img = document.getElementById('img');
+        const img_out = document.getElementById('img_out');
+        if (img.files && img.files[0]) {
             img.onload = () => {
                 URL.revokeObjectURL(img.src);  // no longer needed, free memory
             }
             // Set up reader and call async read function
             const reader = new FileReader();
-            reader.readAsDataURL(this.files[0]);
+            reader.readAsDataURL(img.files[0]);
             // Wait for data to load, and then call blur request and set image
             reader.addEventListener('load', (e) => {
-                img.src = URL.createObjectURL(this.files[0]); // set src to blob url
 
                 const formData = new FormData();
                 const fileInput = document.querySelector('#img');
@@ -33,8 +34,8 @@ window.addEventListener('load', function() {
                 .then(response => {
                     console.log('Server response:', response);
                     response.text().then(function(body) {
-                        console.log(body); // this will be a string
-                        img.src = "data:image/png;base64, " + body
+                        console.log("BODY: ", body); // this will be a string
+                        img_out.src = "data:image/png;base64, " + body
                       }
                     )
                 })
@@ -44,4 +45,19 @@ window.addEventListener('load', function() {
             })            
         }
     });
+
+    // Adding event listener to img loads
+    document.getElementById('img').addEventListener('change', function() {
+        document.getElementById('in_img_1').src = URL.createObjectURL(document.getElementById('img').files[0]); 
+    })
+    document.getElementById('img2').addEventListener('change', function() {
+        document.getElementById('in_img_2').src = URL.createObjectURL(document.getElementById('img2').files[0]); 
+    })
+    document.getElementById('t_img1').addEventListener('change', function() {
+        document.getElementById('in_img_3').src = URL.createObjectURL(document.getElementById('t_img1').files[0]); 
+    })
+    document.getElementById('t_img2').addEventListener('change', function() {
+        document.getElementById('in_img_4').src = URL.createObjectURL(document.getElementById('t_img2').files[0]); 
+    })
 });
+
